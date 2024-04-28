c4f95a084740b8e8d5b87a758a01a7b597d7dbb1
AnaghaJn21 <anaghajayan21@gmail.com>
2024-04-28 14:34:11 +0530

    Modified Sergeant view

---
+++ gectfma/lib/View_Complaints/Sergeant/sergeant_approved_complaint.dart
@@ -0,0 +1,198 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:gectfma/Requirements/Headings.dart';
+import 'package:gectfma/View_Complaints/Sergeant/sergeant_view_all_complaint.dart';
+import '../../Requirements/DetailFields.dart';
+import '../../Requirements/TopBar.dart';
+
+class SergeantApprovedComplaint extends StatefulWidget {
+  final String dept;
+  final String id;
+  const SergeantApprovedComplaint(
+      {super.key, required this.dept, this.id = ""});
+
+  @override
+  State<SergeantApprovedComplaint> createState() =>
+      _SergeantApprovedComplaintState();
+}
+
+/*
+This page accessed when complaint approved but staff has not yet been assigned
+ */
+TextEditingController contactController = TextEditingController();
+TextEditingController descController = TextEditingController();
+TextEditingController hodController = TextEditingController();
+TextEditingController titleController = TextEditingController();
+TextEditingController natureController = TextEditingController();
+TextEditingController statusController = TextEditingController();
+TextEditingController urgencyController = TextEditingController();
+TextEditingController assignedstaffNameController = TextEditingController();
+TextEditingController assignedstaffNumberController = TextEditingController();
+String assignedStaffName = "";
+String assignedStaffNumber = "";
+String status = "";
+
+class _SergeantApprovedComplaintState extends State<SergeantApprovedComplaint> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    if (widget.id != "") {
+      viewComplaint(widget.id, widget.dept);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          TopBar(
+            goto: () {
+              Navigator.of(context).pop();
+            },
+            dept: "DEPARTMENT OF " + widget.dept,
+            iconLabel: 'Go Back',
+            title: "${widget.id}".toUpperCase(),
+            icon: Icons.arrow_back,
+          ),
+          Headings(title: "Department Details*"),
+          DetailFields(
+            isEnable: false,
+            hintText: widget.dept.toUpperCase(),
+          ),
+          DetailFields(
+            isEnable: false,
+            hintText: "Name of HOD",
+            controller: hodController,
+          ),
+          DetailFields(
+            isEnable: false,
+            hintText: "Contact No",
+            controller: contactController,
+          ),
+          Headings(title: "Complaint Details*"),
+          DetailFields(
+              isEnable: false,
+              controller: natureController,
+              hintText: "Nature"),
+          DetailFields(
+              isEnable: false,
+              controller: statusController,
+              hintText: "Status"),
+          DetailFields(
+            isEnable: false,
+            controller: titleController,
+            hintText: "Title",
+          ),
+          DetailFields(
+            isEnable: false,
+            controller: descController,
+            hintText: "Description",
+          ),
+          Headings(title: "Additional documents*"),
+          //Image To be added
+          DetailFields(isEnable: false, hintText: "Image"),
+          SizedBox(
+            height: 20,
+          ),
+          Headings(title: "Urgency level*"),
+          DetailFields(
+            isEnable: false,
+            controller: urgencyController,
+            hintText: "Level",
+          ),
+          Headings(title: "Assigned Staff Details"),
+          DetailFields(
+            hintText: "Assigned Staff Name",
+            controller: assignedstaffNameController,
+          ),
+          DetailFields(
+            hintText: "Assigned Staff Number",
+            controller: assignedstaffNumberController,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                assignedStaffName = assignedstaffNameController.text;
+                assignedStaffNumber = assignedstaffNumberController.text;
+                status = "assigned";
+              });
+              updateAssignedStaff(widget.id, widget.dept);
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.brown[50],
+              backgroundColor: Colors.brown[800], // Text color
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // Corner radius
+              ),
+              minimumSize: Size(150, 50), // Width and height
+            ),
+            child: Text('UPDATE DETAILS'),
+          ),
+          SizedBox(
+            height: 10,
+          )
+        ],
+      ),
+    )));
+  }
+}
+
+Future<Map<String, dynamic>> viewComplaint(String id, String dept) async {
+  try {
+    CollectionReference collectionRef =
+        FirebaseFirestore.instance.collection(dept);
+
+    DocumentSnapshot documentSnapshot = await collectionRef.doc(id).get();
+
+    if (documentSnapshot.exists) {
+      Map<String, dynamic> data = {
+        'contact': documentSnapshot['contact'],
+        'desc': documentSnapshot['desc'],
+        'hod': documentSnapshot['hod'],
+        //image
+        'nature': documentSnapshot['nature'],
+        'status': documentSnapshot['status'],
+        'title': documentSnapshot['title'],
+        'urgency': documentSnapshot['urgency'],
+      };
+      contactController.text = data['contact'];
+      descController.text = data['desc'];
+      hodController.text = data['hod'];
+      natureController.text = data['nature'];
+      statusController.text = data['status'];
+      titleController.text = data['title'];
+      urgencyController.text = data['urgency'];
+      return data;
+    } else {
+      print('Document not found');
+      return {};
+    }
+  } catch (e) {
+    // Handle errors
+    print("Error getting data: $e");
+    return {};
+  }
+}
+
+Future<void> updateAssignedStaff(String id, String dept) async {
+  try {
+    CollectionReference collectionRef =
+        FirebaseFirestore.instance.collection(dept);
+
+    // Update the document
+    await collectionRef.doc(id).update({
+      'assigned_staff': assignedStaffName,
+      'assigned_staff_no': assignedStaffNumber,
+      'status': status,
+    });
+  } catch (e) {
+    // Handle errors
+    print("Error updating data: $e");
+  }
+}
---
+++ gectfma/lib/View_Complaints/Sergeant/sergeant_view_all_complaint.dart
@@ -0,0 +1,191 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:gectfma/View_Complaints/Sergeant/sergeant_approved_complaint.dart';
+import 'package:gectfma/Requirements/Headings.dart';
+import 'package:gectfma/Requirements/TopBar.dart';
+import 'package:gectfma/View_Complaints/view_complaint.dart';
+
+class SergeantViewAllComplaint extends StatefulWidget {
+  final String status;
+  final int total;
+  const SergeantViewAllComplaint(
+      {super.key, required this.total, this.status = ""});
+
+  @override
+  State<SergeantViewAllComplaint> createState() =>
+      _SergeantViewAllComplaintState();
+}
+
+class _SergeantViewAllComplaintState extends State<SergeantViewAllComplaint> {
+  List<Map<String, dynamic>>? temp = [];
+  List<Map<String, dynamic>>? filteredData;
+  List<String>? filteredIds = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: <Widget>[
+          TopBar(
+            goto: () {
+              Navigator.of(context).pop();
+            },
+            dept: "WELCOME Sergeant",
+            iconLabel: 'Go Back',
+            title: "TOTAL " +
+                "${widget.status == "" ? "" : widget.status}" +
+                " COMPLAINTS ${widget.total}",
+            icon: Icons.arrow_back,
+          ),
+          SizedBox(height: 10),
+          for (String dept in [
+            "arch",
+            "ce",
+            "che",
+            "cse",
+            "ece",
+            "ee",
+            "me",
+            "pe"
+          ])
+            Column(
+              children: [
+                Headings(title: "Department Of ${dept.toUpperCase()}"),
+                FutureBuilder<List<Map<String, dynamic>>>(
+                  future: getData(dept, widget.status),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text("Error: ${snapshot.error}"));
+                    } else {
+                      temp = snapshot.data;
+                      filteredData = temp;
+                      return Column(
+                        children: filteredData!.map((complaintData) {
+                          return eachComplaint(dept, complaintData);
+                        }).toList(),
+                      );
+                    }
+                  },
+                ),
+                SizedBox(height: 10),
+              ],
+            ),
+        ],
+      ),
+    )));
+  }
+
+  Widget eachComplaint(String dept, Map<String, dynamic> complaintData) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          // children: filteredIds!.map((complaint) {
+          //   return Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.brown[100],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                onTap: () {
+                  if (widget.status == 'approved') {
+                    Navigator.of(context)
+                        .pushReplacement(MaterialPageRoute(builder: (context) {
+                      return SergeantApprovedComplaint(
+                        dept: dept,
+                        id: complaintData['id'],
+                      );
+                    }));
+                  } else {
+                    Navigator.of(context)
+                        .pushReplacement(MaterialPageRoute(builder: (context) {
+                      return ViewComplaint(
+                        designation:
+                            widget.status == "assigned" ? "sergeant" : "",
+                        dept: dept,
+                        id: complaintData['id'],
+                      );
+                    }));
+                  }
+                },
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          complaintData['id'],
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.brown[600],
+                          ),
+                        ),
+                        Text(
+                          complaintData['title'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Status: ${complaintData['status']}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(),
+          ],
+        ));
+  }
+}
+
+Future<List<Map<String, dynamic>>> getData(String dept, String status) async {
+  try {
+    // Get a reference to the collection
+    CollectionReference collectionRef =
+        FirebaseFirestore.instance.collection(dept);
+    Query query = collectionRef;
+    query =
+        query.where('status', whereIn: ['assigned', 'approved', 'completed']);
+    if (status.isNotEmpty) {
+      query = query.where('status', isEqualTo: status);
+    }
+    // Query the collection for all documents
+    QuerySnapshot querySnapshot = await query.get();
+
+    // Extract data from each document
+    List<Map<String, dynamic>> data =
+        querySnapshot.docs.map((DocumentSnapshot doc) {
+      // Access fields within the document
+      String title = doc['title'];
+      String id = doc['id'];
+      String status = doc['status'];
+
+      // Return a map representing the document's data
+      return {
+        'title': title,
+        'id': id,
+        'status': status,
+      };
+    }).toList();
+    // Return the list of document data
+    return data;
+  } catch (e) {
+    // Handle errors
+    print("Error getting data: $e");
+    return [];
+  }
+}
